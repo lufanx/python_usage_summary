1f49df3537fa0571ca1f6f41e902ff0f6bdbb9e3
lu fan <554001112@qq.com>
2018-01-11 14:23:14 +0800

    add about python list build in function test.
    Signed-off-by: lu fan <554001112@qq.com>

---
+++ list/list_in_build.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def main():
+    test_list = [1, 4, 6, 'zhangsan', 9, 'lisi']
+    list_operate(test_list)
+
+def list_operate(test_list):
+    
+
+
+if __name__ == "__main__":
+    main()
