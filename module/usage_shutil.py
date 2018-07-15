5710d3ce519d530c5d913c3268b76c200745efe6
凡路 <fanlu@fanludeMacBook-Pro.local>
2018-07-15 12:42:57 +0800

    usage_shutil.py: fix this file
    add:
    	shutil.copy
    	shutil.copyfile
    
    Signed-off-by: 凡路 <fanlu@fanludeMacBook-Pro.local>

--- module/usage_shutil.py
+++ module/usage_shutil.py
@@ -8,10 +8,10 @@ def main():
     """
 
     # will name file copy name_1 file only copy file content
-    #shutil.copyfile('name', 'name_1')
+    shutil.copyfile('name', 'name_1')
 
     # will name file copy name_2 file, include copy file content and file limit
-    #shutil.copy('name', 'name_2')
+    shutil.copy('name', 'name_2')
 
     # will test file zip name.tar.gz file
     shutil.make_archive('name', 'gztar', root_dir='test')
